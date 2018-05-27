ff971df9be48822f9d452b46bbf6801c99a1fac1
fengwz17 <fengwz17@outlook.com>
2018-05-27 17:08:43 +0800

    Update admin.py

--- webchat/bbs/admin.py
+++ webchat/bbs/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from bbs import models
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author', 'pub_date', 'last_modify', 'status')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('article', 'parent_comment', 'comment_type', 'commet', 'user')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'set_as_top_menu', 'positon_index',)
+
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment, CommentAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.UserProfile)
